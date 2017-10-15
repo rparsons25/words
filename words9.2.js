b7f3a757d9e686871438f2dd780edd6e77af3bb1
rparsons25 <rparsons25@gmail.com>
2017-10-14 21:30:37 -0500

    see

--- words9.2.js
+++ words9.2.js
@@ -2,14 +2,14 @@ var xhr = new XMLHttpRequest ();
 xhr.onreadystatechange = function (){
   if (xhr.readyState === 4) {
     console.log(xhr.responseText);
-    
     console.log(typeof xhr.responseText);
     var words1 = JSON.parse (xhr.responseText);
-
-  }
     
+    console.log (words1.length);
+    console.log (words1[20].word.length)
+  }
   };
-console.log (words1[20].word);
+
 xhr.open ('Get', 'level1.json');
 
 xhr.send();
