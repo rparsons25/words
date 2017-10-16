6682e7ab1afcd2c66a6c251a5f17f312377dffcb
rparsons25 <rparsons25@gmail.com>
2017-10-16 11:22:13 -0500

    wrong function

--- words9.2.js
+++ words9.2.js
@@ -8,8 +8,8 @@ xhr.onreadystatechange = function (){
     console.log (words1.length);
     console.log (words1[20].word);
     console.log (words1[20].word.length);
-      findWord(words1);
-      console.log (randomWord);
+      randomNumber();
+      console.log (y);
   }
   };
 
@@ -21,20 +21,13 @@ var y = 0;
 var x = 0;
 
 //random word function
-function findWord(words1) {
-   if (y < 2){
-        randomWord = words1[x][0];
-        console.log (randomWord);
-        g1 = randomWord
-        alert (g1 + ' g1');
-        alert (x);
-       printHTML();
-                }else {
-                    console.log ('Oops! ' +randomWord+ ' has too many points!');
-                    randomNumber();
-                }
-    return randomWord;
+function randomNumber(){
+    x = Math.floor(Math.random()*words1.length);
+    y = words1[x].word;
+    findWord (words1);
+    return y;
 }
+
 
 
     
